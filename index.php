0197882974dfc9254caf48b269c33f05f2e3a70a
KasuganoSoras <kasuganosora@tcotp.cn>
2018-01-14 00:38:22 +0800

    微小的改动
    很惭愧，就做了点微小的改动

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-set_time_limit(10);
+set_time_limit(10);	// 超时设置
 $pathinfo = pathinfo($_SERVER['PHP_SELF']);
 $path = str_replace("/" . $pathinfo['basename'], "", $_SERVER['PHP_SELF']);
 define("ROOT", str_replace("\\", "/", __DIR__));
